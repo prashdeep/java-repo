4fefe8ad0506aa98cafbe377a4b14d70eacba15d
AnaGrau <ana-grau@outlook.com>
2018-01-03 23:16:56 +0200

    Modify class name to Math_Utils and implement substract function

---
+++ src/main/java/com/mycomp/models/Math_Utils.java
@@ -0,0 +1,24 @@
+package com.mycomp.models;
+
+public class Math_Utils {
+
+	/**
+	 * Given two integers return the sum of both
+	 * @param a
+	 * @param b
+	 * @return a + b
+	 */
+	public int add (int a, int b) {
+		return a + b;
+	}
+	
+	/**
+	 * Given two integers return the substraction
+	 * @param a
+	 * @param b
+	 * @return a - b
+	 */
+	public int substract (int a, int  b) {
+		return a - b;
+	}
+}
