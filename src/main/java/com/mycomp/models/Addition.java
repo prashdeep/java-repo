4bb0145dc04e22566ca19dd116827312fb36c5c5
AnaGrau <ana-grau@outlook.com>
2018-01-03 22:56:54 +0200

    Create Addition class
    Create new File using eclipse and modify existing one

---
+++ src/main/java/com/mycomp/models/Addition.java
@@ -0,0 +1,14 @@
+package com.mycomp.models;
+
+public class Addition {
+
+	/**
+	 * Given two integers return the sum of both
+	 * @param a
+	 * @param b
+	 * @return a + b
+	 */
+	public int add (int a, int b) {
+		return a + b;
+	}
+}
--- src/main/java/com/mycomp/models/HelloWorld.java
+++ src/main/java/com/mycomp/models/HelloWorld.java
@@ -3,10 +3,8 @@ package com.mycomp.models;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World.");
-		
-
+		System.out.println("Hola Mundo!! (in Spanish)");
 	}
 
 }
